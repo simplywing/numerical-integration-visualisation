b31f5bd0586a551c51abb315d730d32f7496c16c
simplywing <joel@simplywing.ch>
2020-08-22 12:59:42 +0200

    Zoom & Adjusted Bin rastering

--- main.js
+++ main.js
@@ -13,10 +13,12 @@ scope.xscaling = 50;
 scope.yscaling = 50;
 scope.interval1 = 3.141;
 scope.interval2 = 11;
+scope.zoom = 80;
+scope.intervalCheckbox = true;
 
 function getFunctionResult(num, type, typeArgs){
     if(type === 3){
-        return -Math.sin(num/unit * (1/typeArgs.xScaling)) * typeArgs.yScaling
+        return -Math.sin(num/scope.zoom * (1/typeArgs.xScaling)) * typeArgs.yScaling * scope.zoom * 0.01
     }
 }
 
@@ -39,23 +41,23 @@ function draw(){
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillRect(0, -ch / 2, cw, ch);
 
-    //Unit rastering X
-    for(let i = 0; i < cw; i +=unit){
+    //scope.zoom rastering X
+    for(let i = 0; i < cw; i += parseInt(scope.zoom)){
         line(i, -ch/2, i , ch/2, 1, "rgba(255,255,255,0.4)", ctx);
     }
 
-    //Unit rastering Y
-    for(let i = 0; i < ch/2; i +=unit){
+    //scope.zoom rastering Y
+    for(let i = 0; i < ch/2; i += parseInt(scope.zoom)){
         line(0, i, cw, i, 1, "rgba(255,255,255,0.4)", ctx);
     }
-    for(let i = 0; i > -ch/2; i -=unit){
+    for(let i = 0; i > -ch/2; i -= parseInt(scope.zoom)){
         line(0, i, cw, i, 1, "rgba(255,255,255,0.4)", ctx);
     }
 
     //function
     for(let i = 0; i < cw; i++){
         let args = {yScaling: (scope.yscaling/50) * 300, xScaling: scope.xscaling/50};
-        let h = getFunctionResult(i, 3, args)
+        let h = getFunctionResult(i, 3, args);
         line(i, 0, i, h, 1, "white", ctx);
     }
 
@@ -66,28 +68,64 @@ function draw(){
     // }
 
     //pillars
-    for(let i = -bin/2; i < cw; i +=bin){
+    // for(let i = -bin/2; i < cw; i +=bin){
+    //     let args = {yScaling: (scope.yscaling/50) * 300, xScaling: scope.xscaling/50};
+    //     let h = getFunctionResult(i, 3, args);
+
+    //     //pillar fill
+    //     ctx.fillStyle = "rgba(255,0,0,0.5)";
+    //     ctx.fillRect(i-bin/2, 0, bin, h);
+
+    //     //lines, left & right
+    //     line(i-bin/2, 0, i-bin/2, h, 3, "rgb(255,0,0)", ctx);
+    //     line(i+bin/2, 0, i+bin/2, h, 3, "rgb(255,0,0)", ctx);
+
+    // }
+
+    //pillars Fullscreen
+    // let binSize = cw / bin;
+    // for(let i = -binSize/2; i < cw; i += binSize){
+    //     let args = {yScaling: (scope.yscaling/50) * 300, xScaling: scope.xscaling/50};
+    //     let h = getFunctionResult(i, 3, args);
+
+    //     //pillar fill
+    //     ctx.fillStyle = "rgba(255,0,0,0.5)";
+    //     ctx.fillRect(i-binSize/2, 0, binSize, h);
+
+    //     //lines, left & right
+    //     line(i-binSize/2, 0, i-binSize/2, h, 3, "rgb(255,0,0)", ctx);
+    //     line(i+binSize/2, 0, i+binSize/2, h, 3, "rgb(255,0,0)", ctx);
+
+    // }
+
+    //pillars inside Interval
+    let binRange = (scope.interval2 - scope.interval1) * scope.zoom;
+    let binSize = binRange / bin;
+    let binStart = scope.interval1 * scope.zoom;
+    for(let i = binSize/2 + binStart; i < binRange + binStart; i += binSize){
         let args = {yScaling: (scope.yscaling/50) * 300, xScaling: scope.xscaling/50};
-        let h = getFunctionResult(i, 3, args)
+        let h = getFunctionResult(i, 3, args);
 
-        //pillar fill
-        ctx.fillStyle = "rgba(255,0,0,0.5)";
-        ctx.fillRect(i-bin/2, 0, bin, h);
+        if(scope.intervalCheckbox){
+            //pillar fill
+            ctx.fillStyle = "rgba(255,0,0,0.5)";
+            ctx.fillRect(i-binSize/2, 0, binSize, h);
 
-        //lines, left & right
-        line(i-bin/2, 0, i-bin/2, h, 3, "rgb(255,0,0)", ctx);
-        line(i+bin/2, 0, i+bin/2, h, 3, "rgb(255,0,0)", ctx);
-
+            //lines, left & right
+            line(i-binSize/2, 0, i-binSize/2, h, 2, "rgb(255,0,0)", ctx);
+            line(i+binSize/2, 0, i+binSize/2, h, 2, "rgb(255,0,0)", ctx);
+        }
     }
-    
+
+
     //Interval
     if(scope.intervalCheckbox){
-        let xPosInterval1 = parseInt(scope.interval1 * unit);
-        let xPosInterval2 = parseInt(scope.interval2 * unit);
+        let xPosInterval1 = parseInt(scope.interval1 * scope.zoom);
+        let xPosInterval2 = parseInt(scope.interval2 * scope.zoom);
         line(xPosInterval1, -ch/2, xPosInterval1, ch/2, 1, "rgb(0,0,255)", ctx);
         line(xPosInterval2, -ch/2, xPosInterval2, ch/2, 1, "rgb(0,0,255)", ctx);
     
-        ctx.fillStyle = "rgba(0,0,255,0.5)";
+        ctx.fillStyle = "rgba(0,0,255,0.25)";
         ctx.fillRect(xPosInterval1, -ch/2, xPosInterval2 - xPosInterval1, ch);    
     }
 
